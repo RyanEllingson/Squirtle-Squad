516bff201bc692c4d789d13366ffdf19fa83a381
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2019-11-28 20:58:36 -0600

    route now generated using locations in user input fields; fixed issue with brewery finder

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,14 +3,35 @@ const destEls = document.querySelectorAll(".search-js");
 // Select the submit button
 const buttonEl = document.getElementById("button");
 const mapEl = document.getElementById("map-img");
+const stateSymbols = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"];
+const stateNames = ["alabama","alaska","arizona","arkansas","california","colorado","connecticut","delaware","florida","georgia","hawaii","idaho","illinois","indiana","iowa","kansas","kentucky","louisiana","maine","maryland","massachusetts","michigan","minnesota","mississippi","missouri","montana","nebraska","nevada","new_hampshire","new_jersey","new_york","north_carolina","north_dakota","ohio","oklahoma","oregon","pennsylvania","rhode_island","south_carolina","south_dakota","tennessee","texas","utah","vermont","virginia","washington","west_virginia","wisconsin","wyoming"];
+
+// Function transforms state symbol returned by mapquest to state name understandable by brewery API
+function convertState(stateAbbv) {
+    return stateNames[stateSymbols.indexOf(stateAbbv)];
+}
 
 // Function that will grab all the input values from the destEls array
 function getLocationInputs(){
+    let locationArray = [];
     for (let i = 0; i < destEls.length; i++) {
         const currentDest = destEls[i].value;
-        // Call getBreweries function to get the breweries at the current destination in the array
-        getBreweries(currentDest);    
+        if (currentDest !== "") {
+            // Extract city name from user input and use in brewery get request
+            let queryURL = "http://www.mapquestapi.com/search/v2/radius?key=QE84xF6fPwGPtqLDtyk7AmK1dcKhwF5g&maxMatches=1&origin=" + currentDest;
+            axios.get(queryURL)
+            .then(function(response) {
+                console.log(response);
+                const cityState = [response.data.origin.adminArea5,convertState(response.data.origin.adminArea3)];
+                // Call getBreweries function to get the breweries at the current destination in the array
+                getBreweries(cityState); 
+            })
+          
+
+            locationArray.push(currentDest);
+        } 
     }
+    getRoute(locationArray);
 }
 
 // Add event listener to the submit button and when clicked run the getLocationInputs function
@@ -18,15 +39,15 @@ buttonEl.addEventListener("click", getLocationInputs);
 
 // Function that will find all the breweries at a certain location by using openBreweryDB's API
 function getBreweries(currentLocation) {
-    let queryURL = "https://api.openbrewerydb.org/breweries?by_city=" + currentLocation;
+    const city = currentLocation[0];
+    const state = currentLocation[1];
+    let queryURL = "https://api.openbrewerydb.org/breweries?by_city=" + city + "&by_state=" + state;
     axios.get(queryURL)
     .then(function(response){
         console.log(response.data);
     });       
 }
 
-// Problems right now:
-// 1. The response is returning cities in Birmingham????
 
 function getRoute(locations) {
     let locationString = "";
@@ -37,7 +58,6 @@ function getRoute(locations) {
         }
     }
     locationString = locationString.replace(/\s+/g, '');
-    console.log(locationString);
     const queryURL = "https://www.mapquestapi.com/directions/v2/optimizedroute?key=QE84xF6fPwGPtqLDtyk7AmK1dcKhwF5g&json={'locations':[" + locationString + "]}";
     axios.get(queryURL)
     .then(function(response) {
@@ -46,5 +66,3 @@ function getRoute(locations) {
         mapEl.setAttribute("src",mapURL);
     });
 }
-
-getRoute(["Minneapolis, MN","Green Bay, WI","Orland Park, IL"]);
